c3ef3c4c2f8de25cd41720c6862bffae208aa1b6
rg3915 <regis42santos@gmail.com>
2021-03-15 01:42:50 -0300

    Override list method.

--- tourism/core/api/viewsets.py
+++ tourism/core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from tourism.core.models import TouristSpot
 from .serializers import TouristSpotSerializer
@@ -13,3 +14,9 @@ class TouristSpotViewSet(ModelViewSet):
     def get_queryset(self):
         # Return only approved TouristSpot.
         return TouristSpot.objects.filter(approved=True)
+
+    def list(self, request, *args, **kwargs):
+        # Você pode sobrescrever o método list.
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
