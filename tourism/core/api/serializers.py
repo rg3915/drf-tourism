575caef5212fa7d44e35c77c54e784b079368f3f
rg3915 <regis42santos@gmail.com>
2021-03-24 04:57:34 -0300

    ManyToMany relationships

--- tourism/core/api/serializers.py
+++ tourism/core/api/serializers.py
@@ -4,11 +4,13 @@ from rest_framework.serializers import ModelSerializer
 from tourism.address.api.serializers import AddressSerializer
 from tourism.attraction.api.serializers import AttractionSerializer
 from tourism.core.models import TouristSpot
+from tourism.attraction.models import Attraction
 
 
 class TouristSpotSerializer(ModelSerializer):
+    # attractions = AttractionSerializer(many=True, read_only=True)
     attractions = AttractionSerializer(many=True)
-    address = AddressSerializer()
+    address = AddressSerializer(read_only=True)
     full_description = SerializerMethodField()
 
     class Meta:
@@ -26,6 +28,19 @@ class TouristSpotSerializer(ModelSerializer):
             'rattings',
             'address',
         )
+        read_only_fields = ('comments', 'rattings')
+
+    def create_attractions(self, attractions, tourist_spot):
+        for attraction in attractions:
+            obj = Attraction.objects.create(**attraction)
+            tourist_spot.attractions.add(obj)
+
+    def create(self, validated_data):
+        attractions = validated_data['attractions']
+        del validated_data['attractions']
+        tourist_spot = TouristSpot.objects.create(**validated_data)
+        self.create_attractions(attractions, tourist_spot)
+        return tourist_spot
 
     def get_full_description(self, obj):
         return f'{obj.name} - {obj.description}'
